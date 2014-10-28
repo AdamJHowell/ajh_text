380003ce41908750ec3a9f7e5b686f461a9f1052
Adam Howell <bigguywhokills@yahoo.com>
2014-10-28 15:09:26 -0600

    Initial commit
    Initial commit that holds the basic framework, but no actual code.

---
+++ ajh_text.cpp
@@ -0,0 +1,22 @@
+/*
+ajh_text.cpp  2014-10-28  Adam J. Howell
+This program will grep, awk, and sed.
+*/
+
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+
+const char DELIMITER = '\n';					// The delimiter that I use with getline.
+const int IGNORE = 4096;						// I use this when clearing the input buffer.
+
+
+int main( void )
+{
+
+}
